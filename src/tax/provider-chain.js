8d536e0848b40dcd818d1010571a445452c1100b
enjohn99 <EONNIGELJOHN@GMAIL.COM>
2026-09-22 18:46:28 -0500

    refactor: use provider-neutral tax chain

--- src/tax/provider-chain.js
+++ src/tax/provider-chain.js
@@ -1,11 +1,7 @@
 import { JurisdictionRulesTaxProvider } from "./jurisdiction-rules.js";
-import { StripeTaxProvider } from "./stripe-tax.js";
 
-export function createTaxProviders(env = process.env) {
-  const providers = [];
-  if (env.STRIPE_SECRET_KEY) {
-    providers.push(new StripeTaxProvider({ secretKey: env.STRIPE_SECRET_KEY }));
-  }
-  providers.push(new JurisdictionRulesTaxProvider());
-  return providers;
+export function createTaxProviders() {
+  return [
+    new JurisdictionRulesTaxProvider(),
+  ];
 }
